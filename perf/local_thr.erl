6816253ec5aceacd0367fefe34d7afbddc53cf00
Yurii Rashkovskii <yrashk@gmail.com>
2011-02-17 07:40:04 -0800

    Added some draft throughput perf tests

---
+++ perf/local_thr.erl
@@ -0,0 +1,23 @@
+#! /usr/bin/env escript
+%%! -smp enable -pa ebin
+
+main([BindTo,MessageSizeStr,MessageCountStr]) ->
+    {MessageSize, _} = string:to_integer(MessageSizeStr),
+    {MessageCount, _} = string:to_integer(MessageCountStr),
+    {ok, Context} = ezmq:context(),
+    {ok, Socket} = ezmq:socket(Context, sub),
+    ok = ezmq:setsockopt(Socket,subscribe, <<>>),
+    ok = ezmq:bind(Socket, BindTo),
+    {Elapsed, _} = timer:tc(fun () ->
+                                    [ {ok, _} = ezmq:brecv(Socket) || _I <- lists:seq(1,MessageCount) ]
+                            end,[]),
+    
+    Throughput = MessageCount / Elapsed * 1000000,
+    Megabits = Throughput * MessageSize * 8,
+
+    io:format("message size: ~p [B]~n"
+              "message count: ~p~n"
+              "mean throughput: ~p [msg/s]~n"
+              "mean throughput: ~p [Mb/s]~n",
+              [MessageSize, MessageCount, Throughput, Megabits]).   
+    
---
+++ perf/remote_thr.erl
@@ -0,0 +1,11 @@
+#! /usr/bin/env escript
+%%! -smp enable -pa ebin
+
+main([ConnectTo,MessageSizeStr,MessageCountStr]) ->
+    {MessageSize, _} = string:to_integer(MessageSizeStr),
+    {MessageCount, _} = string:to_integer(MessageCountStr),
+    {ok, Context} = ezmq:context(1),
+    {ok, Socket} = ezmq:socket(Context,pub),
+    ezmq:connect(Socket, ConnectTo),
+    Msg = list_to_binary(lists:duplicate(MessageSize, 0)),
+    [ ezmq:send(Socket, Msg) || _I <- lists:seq(1, MessageCount) ].    
