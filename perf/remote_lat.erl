d712845824dc708d422e33ed9a19504121f2310a
Evax Software <contact@evax.fr>
2011-02-18 14:07:33 +0100

    Add {remote,local}_lat perf tests

---
+++ perf/remote_lat.erl
@@ -0,0 +1,25 @@
+#! /usr/bin/env escript
+%%! -smp enable -pa ebin
+
+main([ConnectTo,MessageSizeStr,RoundtripCountStr]) ->
+    {MessageSize, _} = string:to_integer(MessageSizeStr),
+    {RoundtripCount, _} = string:to_integer(RoundtripCountStr),
+    {ok, Context} = ezmq:context(),
+    {ok, Socket} = ezmq:socket(Context, req),
+    ok = ezmq:connect(Socket, ConnectTo),
+    Msg = list_to_binary(lists:duplicate(MessageSize, 0)),
+    Do = fun() ->
+            ezmq:send(Socket, Msg),
+            {ok, Msg} = ezmq:brecv(Socket)
+        end,
+
+    {Elapsed, _} = timer:tc(fun () ->
+                [ Do() || _I <- lists:seq(1,RoundtripCount) ]
+        end,[]),
+
+    Latency = Elapsed / (RoundtripCount * 2),
+
+    io:format("message size: ~p [B]~n"
+              "roundtrip count: ~p~n"
+              "average latency: ~p [us]~n",
+              [MessageSize, RoundtripCount, Latency]).
