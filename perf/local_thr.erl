364d1d986b47c53a9c8b1046df4188536c6fcfaa
Evax Software <contact@evax.fr>
2011-02-18 14:10:24 +0100

    Revert unmotivated {remote,local}_thr perf tests changes

--- perf/local_thr.erl
+++ perf/local_thr.erl
@@ -1,24 +1,23 @@
 #! /usr/bin/env escript
 %%! -smp enable -pa ebin
 
-main([ConnectTo,MessageSizeStr,MessageCountStr]) ->
+main([BindTo,MessageSizeStr,MessageCountStr]) ->
     {MessageSize, _} = string:to_integer(MessageSizeStr),
     {MessageCount, _} = string:to_integer(MessageCountStr),
     {ok, Context} = ezmq:context(),
     {ok, Socket} = ezmq:socket(Context, sub),
     ok = ezmq:setsockopt(Socket,subscribe, <<>>),
-    ok = ezmq:connect(Socket, ConnectTo),
-    % use an extra message to synchronize
-    {ok, <<"start">>} = ezmq:brecv(Socket),
+    ok = ezmq:bind(Socket, BindTo),
     {Elapsed, _} = timer:tc(fun () ->
-        [ {ok, _} = ezmq:brecv(Socket) || _I <- lists:seq(1,MessageCount) ]
-        end,[]),
-
-    Throughput = MessageCount / (Elapsed / 1000000),
-    Megabits = Throughput * MessageSize * 8 / 1000000,
+                                    [ {ok, _} = ezmq:brecv(Socket) || _I <- lists:seq(1,MessageCount) ]
+                            end,[]),
+    
+    Throughput = MessageCount / Elapsed * 1000000,
+    Megabits = Throughput * MessageSize * 8,
 
     io:format("message size: ~p [B]~n"
               "message count: ~p~n"
               "mean throughput: ~p [msg/s]~n"
-              "mean throughput: ~p [Mbit/s]~n",
-              [MessageSize, MessageCount, Throughput, Megabits]).
+              "mean throughput: ~p [Mb/s]~n",
+              [MessageSize, MessageCount, Throughput, Megabits]).   
+    
--- perf/remote_thr.erl
+++ perf/remote_thr.erl
@@ -1,13 +1,11 @@
 #! /usr/bin/env escript
 %%! -smp enable -pa ebin
 
-main([BindTo,MessageSizeStr,MessageCountStr]) ->
+main([ConnectTo,MessageSizeStr,MessageCountStr]) ->
     {MessageSize, _} = string:to_integer(MessageSizeStr),
     {MessageCount, _} = string:to_integer(MessageCountStr),
     {ok, Context} = ezmq:context(1),
     {ok, Socket} = ezmq:socket(Context,pub),
-    ezmq:bind(Socket, BindTo),
-    io:get_line("Press enter when subscriber's ready"),
-    ezmq:send(Socket, <<"start">>),
+    ezmq:connect(Socket, ConnectTo),
     Msg = list_to_binary(lists:duplicate(MessageSize, 0)),
-    [ ezmq:send(Socket, Msg) || _I <- lists:seq(1, MessageCount) ].
+    [ ezmq:send(Socket, Msg) || _I <- lists:seq(1, MessageCount) ].    
